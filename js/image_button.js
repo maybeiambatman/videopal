44d3b58ea0812d058e2b7bbefc5ce0a09fd51132
maybeiambatman <irtezafall2010@gmail.com>
2014-11-12 17:17:54 -0600

    Added js file that dynamically changes background

---
+++ js/image_button.js
@@ -0,0 +1,4 @@
+$(document).on('click', '.left-button', function(){
+  $("body").css("background", "repeating-linear-gradient(90deg,#5B6B66,#5B6B66 25%,#96908C 25%,#96908C 50%,#A391BC 50%,#A391BC 75%,#151515 75%,#151515 100%)");
+  console.log("What up");
+});
